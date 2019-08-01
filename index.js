ea07b3971f7bd625d0004067427b48579b31d491
alexandr.kolodko <cm-brain@yandex.ru>
2019-08-01 12:48:09 +0300

    Update loading reducer

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-const initState = {
-  loading: []
-}
+const initState = {}
 
-const loadingReducer = (state = initState, action) => {
+export const loading = (state = initState, action) => {
   const { type } = action;
-  const matches = /(.*)_(REQUEST|SUCCESS|FAILURE)/.exec(type);
 
-  if (!matches) return state;
+  const matches = /[\w\/@\-\_]+(REQUEST|RECEIVE|ERROR)/gi.exec(type);
 
-  return {
-    ...state,
-    loading: [
-      ...state.loading,
-      'test'
-    ]
-  };
+  if (!matches) {
+    return { ...state };
+  }
+
+  let key;
+  key = type.match(/(?<=(REQUEST|RECEIVE|FAILURE)_)\w+/) || type.match(/(?<=(REQUEST|RECEIVE|FAILURE)_)\w+(?=(_ERROR))/);
+
+  if (key) {
+    const entity = {
+      errors: []
+    };
+
+    let actionEntity = key[0];
+
+    if (key[0].match(/\w+(?=_ERROR)/)) {
+      actionEntity = actionEntity.match(/\w+(?=_ERROR)/)[0];
+      actionEntity = actionEntity.toLowerCase();
+      entity.isFetching = false;
+      entity.isFetched = true;
+      entity.errors = state[entity] ? [...state[entity].errors, action.error] : [action.error]
+    } else {
+      const actionType = key[1];
+      actionEntity = actionEntity.toLowerCase();
+      if(actionType === 'REQUEST') {
+        entity.isFetching = true;
+        entity.isFetched = false;
+        entity.errors = []
+      } else if (actionType === 'RECEIVE') {
+        entity.isFetching = false;
+        entity.isFetched = true;
+        entity.errors = []
+      }
+    }
+    return {
+      ...state,
+      [actionEntity]: entity
+    };
+  }
+  return { ...state }
 };
-
-export default loadingReducer;
